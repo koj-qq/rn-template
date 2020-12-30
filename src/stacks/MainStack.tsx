a8e47c38da5195d208fe7b18dc9284d0a642cb95
chj_damon <chjdamon@gmail.com>
2020-12-30 19:38:25 +0800

    chore: 升级组件库版本

--- src/stacks/MainStack.tsx
+++ src/stacks/MainStack.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
+import { CardStyleInterpolators, createStackNavigator, StackHeaderLeftButtonProps } from '@react-navigation/stack';
+import { TouchableOpacity } from 'react-native';
+import { Theme, useTheme, helpers, Icon } from '@td-design/react-native';
 
 import Homepage from '../screens/homepage';
 
+const { px } = helpers;
 const Stack = createStackNavigator();
 export const MainStack = () => {
+  const theme = useTheme<Theme>();
   return (
     <Stack.Navigator
       initialRouteName="Homepage"
@@ -12,18 +16,18 @@ export const MainStack = () => {
       headerMode="screen"
       // Stack下每个screen都会共享的配置
       screenOptions={{
-        // headerTitleStyle: {
-        //   fontWeight: '500',
-        //   color: theme.colors.primaryTextColor,
-        //   fontSize: px(18),
-        // },
+        headerTitleStyle: {
+          fontWeight: '500',
+          color: theme.colors.primaryTextColor,
+          fontSize: px(18),
+        },
         headerTitleAlign: 'center',
-        // headerLeft: (props: StackHeaderLeftButtonProps) =>
-        //   props.canGoBack && (
-        //     <TouchableOpacity activeOpacity={0.8} onPress={props.onPress} style={{ marginLeft: 0, padding: px(10) }}>
-        //       <Icon name="left" size={px(20)} color={theme.colors.primaryColor} />
-        //     </TouchableOpacity>
-        //   ),
+        headerLeft: (props: StackHeaderLeftButtonProps) =>
+          props.canGoBack && (
+            <TouchableOpacity activeOpacity={0.8} onPress={props.onPress} style={{ marginLeft: 0, padding: px(10) }}>
+              <Icon name="left" size={px(20)} color={theme.colors.primaryColor} />
+            </TouchableOpacity>
+          ),
         gestureEnabled: true,
         gestureDirection: 'horizontal',
         cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
