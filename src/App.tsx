f60804b24ce86638b8ff0b53df97e91e5f67017c
chj_damon <chjdamon@gmail.com>
2020-12-30 18:07:09 +0800

    feat: 集成组件库以及相应的配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from '@td-design/react-native';
 
 import { MainStack } from './stacks/MainStack';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <MainStack />
-      </NavigationContainer>
+      <ThemeProvider>
+        <NavigationContainer>
+          <MainStack />
+        </NavigationContainer>
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 };
--- src/screens/homepage/index.tsx
+++ src/screens/homepage/index.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Box, Text, WhiteSpace, WingBlank } from '@td-design/react-native';
 
 import Container from '@/components/Container';
 
 export default function () {
-  const myButton = (
-    <Icon.Button name="facebook" backgroundColor="#3b5998">
-      Login with Facebook
-    </Icon.Button>
-  );
-
   return (
     <Container>
-      <View style={{ padding: 20 }}>
-        <View>{myButton}</View>
-      </View>
+      <WingBlank>
+        <Box>
+          <Text variant="primaryBody">hello, td-design</Text>
+        </Box>
+        <WhiteSpace />
+        <Box>
+          <Text variant="primaryTipReverse">hello, react-native</Text>
+        </Box>
+      </WingBlank>
     </Container>
   );
 }
