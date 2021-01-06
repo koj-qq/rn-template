2d943fa2511d5faf4b1a05de25cb43d9326ea0f4
chj_damon <chjdamon@gmail.com>
2021-01-06 19:37:03 +0800

    fix: 修复useAsync的测试用例一直卡死的问题
    参见issue: https://github.com/testing-library/react-hooks-testing-library/issues/523

--- src/hooks/useAsync.test.ts
+++ src/hooks/useAsync.test.ts
@@ -1,12 +1,13 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import useAsync from './useAsync';
 
 test('should increment counter after delay', async () => {
-  const { result, waitForNextUpdate } = renderHook(() => useAsync());
+  const { result } = renderHook(() => useAsync());
 
   result.current.incrementAsync(); // async不需要放在act里
-
-  await waitForNextUpdate();
+  act(() => {
+    jest.runAllTimers();
+  });
 
   expect(result.current.count).toBe(1);
 });
