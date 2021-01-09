7304bff5d4efb57601ba71e683dce6fb2654ae69
chj_damon <chjdamon@gmail.com>
2021-01-09 12:06:05 +0800

    feat: 新增对webp的支持
    后面所有图片都转成webp格式

---
+++ readMe.md
@@ -0,0 +1,8 @@
+## 如何将 png 转成 webp
+
+执行 assets 下的`cwebp.sh`:
+
+```code
+cd assets/
+sh ./cwebp.sh
+```
--- src/screens/homepage/index.tsx
+++ src/screens/homepage/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Text, WhiteSpace } from '@td-design/react-native';
 import Container from '@/components/Container';
+import { Image } from 'react-native';
 
 export default function Homepage() {
   return (
@@ -8,6 +9,7 @@ export default function Homepage() {
       <Box>
         <Text>hello, td-design</Text>
         <WhiteSpace />
+        <Image source={require('@/assets/certify_fail.webp')} style={{ width: 200, height: 200 }} />
       </Box>
     </Container>
   );
